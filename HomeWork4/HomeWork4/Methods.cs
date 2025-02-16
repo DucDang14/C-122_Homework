4bca44443fcaa57282ada194fb1742e6e050576c
AnhDang8 <anhduc.dang@mnsu.edu>
2025-02-16 17:37:43 -0600

    HomeWork4
    HomeWork4

---
+++ HomeWork4/HomeWork4/Methods.cs
@@ -0,0 +1,96 @@
+﻿//Written by Duc Anh Dang
+//02/14/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace HomeWork4
+{
+    internal class Methods
+    {
+        //Count words
+        public int countWords(string text)
+        {
+            int count = 1;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        //Flip end char
+            
+        public string flipEndChars(string text)
+        {
+            string msg = "";
+            if (text.Length < 2)
+            {
+                return "Incompatible";
+            }
+            if (text[0] == text[^1])
+            {
+                return "Two's a pair";
+            }
+
+            msg +=  text[^1] + text.Substring(1,text.Length-2) + text[0];
+            
+            return msg;
+        }
+        //Is in order or not
+        public bool isInOrder(string text)
+        {
+            char[] chars = text.ToCharArray();
+            string newText = "";
+            Array.Sort(chars);
+            
+            foreach (char c in chars) { newText += c; }
+            return newText == text;
+        }
+        //First n vowels of a string
+        public string firstNVowels(string text, int n)
+        {
+            string vowels = "aeiou";
+            string msg = "";
+            text.ToLower();
+            if (n >= text.Length)
+            {
+                return "invalid";
+            }
+            else
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    foreach (char ch in vowels)
+                    {
+                        if (ch == text[i])
+                        {
+                           msg += ch;
+                        }
+                        if (msg.Length == n)
+                        {
+                            return msg; 
+                        }
+                    }
+                }
+            }
+            return msg;
+        }
+        //Change every letter to the next letter
+        public string move(string text)
+        {
+            string msg = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                msg += ((char)(text[i] + 1)).ToString();
+            }
+            return msg;
+        }
+    }
+}
---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,47 @@
+﻿//Written by Duc Anh Dang
+//02/14/2025
+using System.Threading.Channels;
+
+namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Methods methods = new Methods();
+            //words count
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Word Count");
+            Console.WriteLine(methods.countWords("Just an example here move along"));
+            Console.WriteLine(methods.countWords("This is a test"));
+            Console.WriteLine(methods.countWords("What an easy task, right"));
+            //Flip end Char
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Flip end Char");
+            Console.WriteLine(methods.flipEndChars("Cat, dog, and mouse."));
+            Console.WriteLine(methods.flipEndChars("ada"));
+            Console.WriteLine(methods.flipEndChars("Ada"));
+            Console.WriteLine(methods.flipEndChars("z"));
+            ////Is in order 
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Is in order ?");
+            Console.WriteLine(methods.isInOrder("abc"));
+            Console.WriteLine(methods.isInOrder("edabit"));
+            Console.WriteLine(methods.isInOrder("123"));
+            Console.WriteLine(methods.isInOrder("xyzz"));
+            //First n vowels of a string
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine(methods.firstNVowels("sharpening skills", 3));
+            Console.WriteLine(methods.firstNVowels("major league", 5));
+            Console.WriteLine(methods.firstNVowels("hoste", 5));
+            ////Change letter to next letter
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine(methods.move("hello"));
+            Console.WriteLine(methods.move("bye"));
+            Console.WriteLine(methods.move("welcome"));
+            
+            Console.ReadKey();
+        }
+    }
+}
